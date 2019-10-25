a46de21fd4fdaf2c70d3941b9e0e56f903015e0b
Hamza Gillani <hamzagillani10@gmail.com>
2019-10-26 01:46:27 +0500

    map integrate

--- MapActivitymodule/app/src/main/java/com/mapActivity/MapsActivity.java
+++ MapActivitymodule/app/src/main/java/com/mapActivity/MapsActivity.java
@@ -1,104 +1,198 @@
 package com.mapActivity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.ActionBar;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    public static final int PERMISSION_REQUEST_CODE=9001;
-    private final int PLAY_SERVICES_ERROR_CODE=9002;
-    private GoogleMap mMap;
+    public static final int PERMISSION_REQUEST_CODE = 9001;
+    private final int PLAY_SERVICES_ERROR_CODE = 9002;
+    private final double ISLAMABAD_LAT = 33.690904;
+    private final double ISLAMABAD_LNG = 73.051865;
+    private GoogleMap mGoogleMap;
     private boolean mLocationisPermissionisGranted;
+    private SupportMapFragment supportMapFragment;
+    private FloatingActionButton floatingActionButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
-    initGoogleMap();
+        setContentView(R.layout.activity_maps);
+        floatingActionButton=findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(View-> {
+
+                if (mGoogleMap!=null){
+
+                    LatLng latLng = new LatLng(ISLAMABAD_LAT, ISLAMABAD_LNG);
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+                    markerOptions.position(latLng).title("Islamabad");
+                    mGoogleMap.addMarker(markerOptions);
+                    mGoogleMap.animateCamera(cameraUpdate, 9000, new GoogleMap.CancelableCallback() {
+                        @Override
+                        public void onFinish() {
+                            Toast.makeText(MapsActivity.this, "Animation Finished", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onCancel() {
+                            Toast.makeText(MapsActivity.this, "Animation Cancelled", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    
+                }
+
+        });
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+
+        supportMapFragment.getMapAsync(this);
+
+        initGoogleMap();
 
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mGoogleMap = googleMap;
+        gotoLocation(ISLAMABAD_LAT, ISLAMABAD_LNG);
+        //LatLng latLng=new LatLng(ISLAMABAD_LAT,ISLAMABAD_LNG);
+        //MarkerOptions markerOptions=new MarkerOptions();
+        //markerOptions.position().title("Islamabad");
+        // mGoogleMap.addMarker(markerOptions);
+
+      /*  mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+    }
+
+    private void gotoLocation(double lat, double lng) {
+        LatLng latLng = new LatLng(lat, lng);
+        MarkerOptions markerOptions = new MarkerOptions();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 14);
+        mGoogleMap.moveCamera(cameraUpdate);
+        mGoogleMap.getUiSettings().setZoomGesturesEnabled(true);
+        //mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        markerOptions.position(latLng).title("Islamabad");
+        mGoogleMap.addMarker(markerOptions);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.maptype_none_id:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.maptype_normal_id:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.maptype_satellite_id:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.maptype_terrain_id:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.maptype_hybrid_id:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void initGoogleMap() {
-        if (isServicesOk()){
-            if (checkLocationPermission()){
+        if (isServicesOk()) {
+            if (checkLocationPermission()) {
                 Toast.makeText(this, "Ready to Map", Toast.LENGTH_SHORT).show();
-            }else {
+            } else {
                 requestLocationPermission();
             }
         }
     }
 
     private boolean checkLocationPermission() {
-        return ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
-                ==PackageManager.PERMISSION_GRANTED;
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
     }
 
     private boolean isServicesOk() {
-        GoogleApiAvailability googleApi=GoogleApiAvailability.getInstance();
+        GoogleApiAvailability googleApi = GoogleApiAvailability.getInstance();
 
-        int result=googleApi.isGooglePlayServicesAvailable(this);
-        if (result== ConnectionResult.SUCCESS){
+        int result = googleApi.isGooglePlayServicesAvailable(this);
+        if (result == ConnectionResult.SUCCESS) {
             return true;
-        }else if(googleApi.isUserResolvableError(result)){
-            Dialog dialog=googleApi.getErrorDialog(this,result,PLAY_SERVICES_ERROR_CODE,task->
-                    Toast.makeText(MapsActivity.this, "Dialog is Cancelled by user", Toast.LENGTH_SHORT).show() );
+        } else if (googleApi.isUserResolvableError(result)) {
+            Dialog dialog = googleApi.getErrorDialog(this, result, PLAY_SERVICES_ERROR_CODE, task ->
+                    Toast.makeText(MapsActivity.this, "Dialog is Cancelled by user", Toast.LENGTH_SHORT).show());
             dialog.show();
-        }else {
+        } else {
             Toast.makeText(MapsActivity.this, "Play Services are Required by this Application...", Toast.LENGTH_SHORT).show();
         }
         return false;
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode==PERMISSION_REQUEST_CODE && grantResults[0] ==PackageManager.PERMISSION_GRANTED ){
-            mLocationisPermissionisGranted=true;
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mLocationisPermissionisGranted = true;
             Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
 
-        }else {
+        } else {
             Toast.makeText(this, "Permission is not Granted ", Toast.LENGTH_SHORT).show();
         }
     }
 
-  private void requestLocationPermission(){
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
-        != PackageManager.PERMISSION_GRANTED){
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_CODE);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
             }
         }
 
